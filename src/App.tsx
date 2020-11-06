57e38194430cfcdf28fa17aecfb7496236599119
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-06 18:07:42 -0500

    hopefully finished prepping material for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { Grid, Container } from '@material-ui/core';
 import './App.css';
+
+const theme = {};
 
 function App() {
   return (
     <>
+    <ThemeProvider theme={theme}>
+    <header></header>
+
+    <body>
+
+      <h1>Well hello there!</h1>
+    </body>
+
+    <footer></footer>
+
+    </ThemeProvider>
+    <CssBaseline />
     </>
   );
 }
